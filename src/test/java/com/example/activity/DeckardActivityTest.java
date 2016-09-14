e4d4e7938403db75589b57a1b4658d8ac298e089
Menny Even Danan <menny@evendanan.net>
2016-09-14 10:24:45 -0400

    tests for failing getExternalCacheDirs

--- src/test/java/com/example/activity/DeckardActivityTest.java
+++ src/test/java/com/example/activity/DeckardActivityTest.java
@@ -1,5 +1,7 @@
 package com.example.activity;
 
+import android.annotation.TargetApi;
+
 import com.example.BuildConfig;
 
 import org.junit.Test;
@@ -17,5 +19,40 @@ public class DeckardActivityTest {
     @Test
     public void testSomething() throws Exception {
         assertTrue(Robolectric.setupActivity(DeckardActivity.class) != null);
+    }
+
+    @Test
+    @Config(sdk = 19)
+    @TargetApi(19)
+    public void testGetExternalCacheDirs19() throws Exception {
+        assertTrue(Robolectric.setupActivity(DeckardActivity.class).getExternalCacheDirs().length > 0);
+    }
+
+    @Test
+    @Config(sdk = 20)
+    @TargetApi(20)
+    public void testGetExternalCacheDirs20() throws Exception {
+        assertTrue(Robolectric.setupActivity(DeckardActivity.class).getExternalCacheDirs().length > 0);
+    }
+
+    @Test
+    @Config(sdk = 21)
+    @TargetApi(21)
+    public void testGetExternalCacheDirs21() throws Exception {
+        assertTrue(Robolectric.setupActivity(DeckardActivity.class).getExternalCacheDirs().length > 0);
+    }
+
+    @Test
+    @Config(sdk = 22)
+    @TargetApi(22)
+    public void testGetExternalCacheDirs22() throws Exception {
+        assertTrue(Robolectric.setupActivity(DeckardActivity.class).getExternalCacheDirs().length > 0);
+    }
+
+    @Test
+    @Config(sdk = 23)
+    @TargetApi(23)
+    public void testGetExternalCacheDirs23() throws Exception {
+        assertTrue(Robolectric.setupActivity(DeckardActivity.class).getExternalCacheDirs().length > 0);
     }
 }
