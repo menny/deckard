7798e7630a01b83da22d6c41c83156f648667b01
Menny Even Danan <menny@evendanan.net>
2016-11-03 14:31:48 -0400

    failing test for fast-scroller

--- src/main/java/com/example/activity/DeckardActivity.java
+++ src/main/java/com/example/activity/DeckardActivity.java
@@ -2,6 +2,8 @@ package com.example.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
 import com.example.R;
 
 public class DeckardActivity extends Activity {
@@ -10,5 +12,7 @@ public class DeckardActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.deckard);
+    ListView listView = (ListView) findViewById(android.R.id.list);
+    listView.setFastScrollEnabled(true);
   }
 }
